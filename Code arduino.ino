4d5663397673d2ae56312794d16a1a6faf87052a
fareanor3 <61111238+fareanor3@users.noreply.github.com>
2021-05-13 13:56:56 +0200

    Update Code arduino.ino

--- Code arduino.ino
+++ Code arduino.ino
@@ -1,47 +1,74 @@
+//fareanor3 Update Code arduino.ino
+//Latest commit 9d23874
+//101 lines (83 sloc)  2.94 KB
+  
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <Arduino_JSON.h>
-
+// wifi set up values
 const char* ssid = "SSID";
 const char* password = "PASSWORD";
+// pin set up values
+const int wifi_led_statue = ; // to define pin number
+const int sun = ; // to define pin number
+const int cloud = ; // to define pin number
+const int rain = ; // to define pin number
+const int snow = ; // to define pin number
+
+// differents configurations
+
+//     | Sun | cloud | rain | snow | total weather id
+// ---------------------------------------------------
+//  1  | X   |       |      |      |   ? to define
+//  2  | X   |   X   |      |      |   ? to define
+//  3  |     |   X   |      |      |   ? to define
+//  4  |     |   X   |  X   |      |   ? to define
+//  5  |     |   X   |      |  X   |   ? to define
+//  6  |     |   X   |  X   |  X   |   ? to define
 
 // Your Domain name with URL path or IP address with path
 String openWeatherMapApiKey = "bf7dcd4f6c9b368dae6cc55b5dbcdbafY"; //api key
 
-// Replace with your country code and city
 String city = "Paris";
 String countryCode = "FR";
 
 // THE DEFAULT TIMER IS SET TO 10 SECONDS FOR TESTING PURPOSES
-// For a final application, check the API call limits per hour/minute to avoid getting blocked/banned
+// For the final application, we gonna check the API call limits per hour/minute to avoid getting blocked/banned
 unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
+// if Timer set to 10 minutes (600000)
 //unsigned long timerDelay = 600000;
 // Set timer to 10 seconds (10000)
 unsigned long timerDelay = 10000;
 
 String jsonBuffer;
 
+// the setup function runs once when you press reset or power the board
 void setup() {
-  Serial.begin(115200);
 
+  pinMode(wifi_led_statue, OUTPUT);
+  pinMode(sun, OUTPUT);
+  pinMode(cloud, OUTPUT);
+  pinMode(rain, OUTPUT);
+  pinMode(snow, OUTPUT);
+
+  Serial.begin(115200); // what does it's mean
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    delay(500); // blink 2 times/s
+    Serial.print("."); // change to a blue leds blink during time connection
   }
   Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
+  Serial.print("Connected to WiFi network with IP Address: "); // change to a triple red blink of all the leds
  
   //Timer set to 10 seconds (timerDelay variable), it will take 10 seconds before publishing the first reading
 }
 
+// the loop function runs over and over again forever
 void loop() {
   // Send an HTTP GET request
-  if ((millis() - lastTime) > timerDelay) {
+  if ((millis() - lastTime) > timerDelay) { // millis meaning ?
     // Check WiFi connection status
     if(WiFi.status()== WL_CONNECTED){
       String serverPath = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + countryCode + "&APPID=" + openWeatherMapApiKey;
@@ -51,8 +78,8 @@ void loop() {
       JSONVar myObject = JSON.parse(jsonBuffer);
   
       // JSON.typeof(jsonVar) can be used to get the type of the var
-      if (JSON.typeof(myObject) == "undefined") { // find a way to show on the box wifi statut
-        Serial.println("Parsing input failed!"); // find a way to show on the box wifi statut
+      if (JSON.typeof(myObject) == "undefined") { // find a way to show on the box fail statut
+        Serial.println("Parsing input failed!"); // find a way to show on the box fail statut
         return;
       }
     
@@ -83,7 +110,7 @@ String httpGETRequest(const char* serverName) {
   // Send HTTP POST request
   int httpResponseCode = http.GET();
   
-  String payload = "{}"; 
+  String payload = "{}"; // payload = find meaning
   
   if (httpResponseCode>0) {
     Serial.print("HTTP Response code: ");
@@ -92,7 +119,7 @@ String httpGETRequest(const char* serverName) {
   }
   else {
     Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
+    Serial.println(httpResponseCode); // find a way to show type error on led. may be by a morse led pulsation
   }
   // Free resources
   http.end();
