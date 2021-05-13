4d695e476be26e3f1d80ffa3cadd91b0fddab47b
fareanor3 <61111238+fareanor3@users.noreply.github.com>
2021-05-13 21:58:16 +0200

    Update Code arduino.ino

--- Code arduino.ino
+++ Code arduino.ino
@@ -6,6 +6,7 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <Arduino_JSON.h>
+
 // wifi set up values
 const char* ssid = "SSID";
 const char* password = "PASSWORD";
@@ -18,14 +19,13 @@ const int snow = ; // to define pin number
 
 // differents configurations
 
-//     | Sun | cloud | rain | snow | total weather id
-// ---------------------------------------------------
-//  1  | X   |       |      |      |   ? to define
-//  2  | X   |   X   |      |      |   ? to define
-//  3  |     |   X   |      |      |   ? to define
-//  4  |     |   X   |  X   |      |   ? to define
-//  5  |     |   X   |      |  X   |   ? to define
-//  6  |     |   X   |  X   |  X   |   ? to define
+//     | Sun | cloud | rain | snow | temperature | total weather id |
+//  1  | X   |       |      |      |             |   ? to define    |
+//  2  | X   |   X   |      |      |             |   ? to define    | 
+//  3  |     |   X   |      |      |             |   ? to define    |
+//  4  |     |   X   |  X   |      |             |   ? to define    |
+//  5  |     |   X   |      |  X   |             |   ? to define    |
+//  6  |     |   X   |  X   |  X   |             |   ? to define    |
 
 // Your Domain name with URL path or IP address with path
 String openWeatherMapApiKey = "bf7dcd4f6c9b368dae6cc55b5dbcdbafY"; //api key
@@ -57,7 +57,12 @@ void setup() {
   Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
     delay(500); // blink 2 times/s
-    Serial.print("."); // change to a blue leds blink during time connection
+    for (int count = 0; count < 3; count++) {
+    digitalWrite(//pin a déterminer , HIGH);
+    delay(500);
+    digitalWrite(//pin a déterminer , LOW)
+    delay(250);
+  }; // change to a blue leds blink during time connection
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: "); // change to a triple red blink of all the leds
@@ -126,3 +131,4 @@ String httpGETRequest(const char* serverName) {
 
   return payload;
 }
+//© 2021 GitHub, Inc.
